6c572bcec31bcbfcf0b7e0f3ed3ad7ce2aed5062
JakubeQ <jakub132346@gmail.com>
2026-02-28 17:06:00 +0100

    LaboV1

--- LaboV1/05_Warunki.py
+++ LaboV1/05_Warunki.py
@@ -1,7 +1,7 @@
-'''Instrukcje warunkowe
+'''Instrukcje warunkowe ID: 05
 Zadanie - listę wartości 'lista' należy rozdzielić
 na trzy listy:
-l1 zawiera elementy mniejsze niż v1
+l1 zawiera elementy mniejsze równe niż v1
 l2 zawiera el. wększe niż v1 i mniejsze niż v2
 l3 zawiera elementy pozostałe'''
 
@@ -14,10 +14,12 @@ indeks = 0
 while indeks < len(lista):
     if lista[indeks] <= v1:
         l1.append(lista[indeks])
-    elif lista[indeks] <= v2:
+    elif lista[indeks] < v2:
         l2.append(lista[indeks])
     else:
         l3.append(lista[indeks])
     indeks += 1
+else:
+    print("a teraz pocałuj się w 'czapkę'")
 
 print('',l1,'\n',l2,'\n',l3)
