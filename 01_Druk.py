33e46783794401cea5fa8c0c3662354c25c08228
JakubeQ <jakub132346@gmail.com>
2026-02-28 16:25:52 +0100

    przyklad funkcji len

---
+++ 01_Druk.py
@@ -0,0 +1,16 @@
+'''Drukowanie tekstu ID 05'''
+print('Oto\n jak się drukuje')
+
+# Wprowadzanie danych
+Imię_17 = input('Wpisz imię i zatwierdź Enter: ')
+print("Witaj",Imię_17)
+
+# Ten znak rozpoczyna komentarz. len() daje długość
+print(len(Imię_17), Imię_17[2:7])
+
+# metoda .count zwraca liczbę wystąpień argumentu
+print(Imię_17.count('a'))
+
+shout = input('good morning my neighbours!')
+print('yes yes', shout, 'too!')
+
