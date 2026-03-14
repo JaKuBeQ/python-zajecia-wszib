f4297cd1ed8653ed272c357f87b89d1873510d7a
JakubeQ <jakub132346@gmail.com>
2026-03-14 17:28:48 +0100

    full zajecia

--- LaboV2/sortowanie.py
+++ LaboV2/sortowanie.py
@@ -11,10 +11,13 @@ tbl = [randint(1,10**5) for i in range(10**4)]
 
 tb2 = tbl.copy()
 tb2.sort()
-t0 = time()
 
+t0 = time()
 tbsort = []
 
 while tbl :
     ix = tbl.index(min(tbl))
     tbsort.append(tbl.pop(ix))
+if tbsort == tb2 :
+    print('ok czas sortowania: ', time() - t0)
+
