3829d72275c593ab3ddfef3dc973f7f00b0baea8
JakubeQ <jakub132346@gmail.com>
2026-03-28 17:12:37 +0100

    zadanie 2

---
+++ LaboV3/merge.py
@@ -0,0 +1,32 @@
+'''
+imie naziwsko: jakub łysiak
+ID: 05
+temat zadania - merge list jako funkcja (funkcja zaplatania)
+'''
+
+from random import randint
+from math import inf
+
+Tb1  = [randint(0, 100) for ix_88 in range(100)]
+Tb2  = [randint(0, 100) for ix_88 in range(105)]
+
+Tb1.sort(reverse=True)
+Tb2.sort(); Tb2.reverse()
+
+def merge(A,B):
+    Splot = []
+    L = len (A); l = 0
+    R = len (B); r = 0
+    while l < L and r < R:
+        if A[l] >  B[r]:
+            Splot.append(A[l])
+            l += 1
+        else:
+            Splot.append(B[r])
+            r += 1\
+
+    Splot += A[l:] + B[r:]
+    return Splot
+
+Odpow = merge(Tb1, Tb2)
+print(Odpow)
