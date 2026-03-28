1097661cd269b0754ae281b448c63686d9678e1c
JakubeQ <jakub132346@gmail.com>
2026-03-28 17:25:45 +0100

    .

--- LaboV3/merge.py
+++ LaboV3/merge.py
@@ -28,5 +28,25 @@ def merge(A,B):
     Splot += A[l:] + B[r:]
     return Splot
 
+def merge2(A,B):
+    A.append(-inf)
+    B.append(-inf)
+    Splot = []
+    L = len (A); l = 0
+    R = len (B); r = 0
+    while l + r < R+L-2:
+        if A[l] >  B[r]:
+            Splot.append(A[l])
+            l += 1
+        else:
+            Splot.append(B[r])
+            r += 1\
+
+        #Splot += A[l:] + B[r:]
+    return Splot
+
 Odpow = merge(Tb1, Tb2)
 print(Odpow)
+
+Odpow2 = merge2(Tb1, Tb2)
+print(Odpow2)
