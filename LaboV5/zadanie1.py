a809300367c66a2d3a1139ef45cf20aea232fbf4
JakubeQ <jakub132346@gmail.com>
2026-05-09 16:40:37 +0200

    zadanie 1 - bucket sort implementation

---
+++ LaboV5/zadanie1.py
@@ -0,0 +1,25 @@
+'''
+imie nazwisko: jakub łysiak
+ID: 05
+temat zadania - sortowanie bucket sort ułamków
+'''
+
+from time import time
+from random import uniform
+
+D = [uniform(0,1) for i_88 in range(10**6)]
+
+B = len(D)//5
+
+Buc = [[] for ir in range(B)]
+
+def srt(A):
+    G = []
+    while A:
+        ind = A.index(min(A))
+        G.append(A.pop(ind))
+    return G
+
+for ir in D:
+    hasH = int(ir*B)
+    Buc[hasH].append(ir)
