b7075a40b39febdd1d0d5cfcb89155f98eff2d29
JakubeQ <jakub132346@gmail.com>
2026-06-13 16:54:04 +0200

    LaboV7 - add implementations for priority queue, Binary Search Tree (BST) with splay operations, bubble sort (two approaches), and comb sort; include additional resources

---
+++ LaboV7/BinaryConverter.py
@@ -0,0 +1,20 @@
+'''
+imie nazwisko: jakub łysiak
+ID: 05
+temat zadania - funkcja rekursyjna, zamieniajacą zapis liczby z systemu dziesiętnego na dwójkowy.
+'''
+
+def convert(n):
+    if n < 2:
+        return str(n)
+    return convert(n//2) + str(n%2)
+
+def convert2(n):
+    if n < 2:
+        return str(n)
+    return convert2(n//8) + str(n%8)
+
+
+
+print("1024 w systemie dwojkowym to ", convert(1024))
+print("729 w systemie osemkowym to ", convert2(729))
